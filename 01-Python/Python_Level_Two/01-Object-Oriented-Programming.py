4041ba0f0ed450eea3b68e793e075a52bc84c5ff
CrazyDi <43439123+CrazyDi@users.noreply.github.com>
2020-04-30 10:54:48 +0300

    OOP

--- 01-Python/Python_Level_Two/01-Object-Oriented-Programming.py
+++ 01-Python/Python_Level_Two/01-Object-Oriented-Programming.py
@@ -1,18 +1,35 @@
-class Sample():
-    pass
-
-
-x = Sample()
-
-print(type(x))
-
-
 class Dog:
-    def __init__(self, breed):
+
+    species = 'mammal'
+
+    def __init__(self, breed, name):
         self.breed = breed
+        self.name = name
 
 
-x = Dog('lab')
+sam = Dog('lab', 'Frankie')
+new_dog = Dog('Golden', 'Cindy')
 
-print(type(x))
-print(x.breed)
+print(type(sam))
+print(sam.breed)
+print(new_dog.breed)
+
+
+class Circle:
+
+    pi = 3.14
+
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * self.pi
+
+    def circumfrence(self):
+        return 2 * self.pi * self.radius
+
+
+mycircle = Circle(10)
+print(mycircle.radius)
+print(mycircle.area())
+print(mycircle.circumfrence())
